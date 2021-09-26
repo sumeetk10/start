7f21aa7b96648f886ca70904561b46ccf6d3727b
sumeetk10 <sumeet.k2022@gmail.com>
2021-09-26 16:18:08 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int num1,num2,sum=0;
+        System.out.println("Sum of two Numbers");
+        System.out.println("Enter 1st number: ");
+        num1 = input.nextInt();
+        System.out.println("Enter 2nd Number: ");
+        num2 = input.nextInt();
+        sum=num1+num2;
+        System.out.println("Sum : " +sum);
+
+    }
+}
+    
+
