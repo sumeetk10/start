e9ca60c6cb4dc5e49594b342e0608309bfecec7c
sumeetk10 <sumeet.k2022@gmail.com>
2021-11-05 22:53:02 +0530

    initial commit

---
+++ first.java
@@ -0,0 +1,12 @@
+
+public class first {
+    
+    public static void main(final String[] args) {
+       int avalue;
+
+       avalue=42;
+       System.out.println(avalue);
+    }
+
+   
+}
---
+++ variable.java
@@ -0,0 +1,18 @@
+public class variable {
+    public static void main(String[] args) {
+    int aValue=42;
+    short aShort=93;
+    long aLong=8304;
+    float aFloat=5.432f;
+    char aChar='v';
+    byte aByte=127;
+
+    System.out.println(aValue);
+    System.out.println(aShort);    
+    System.out.println(aLong);    
+    System.out.println(aFloat);    
+    System.out.println(aChar);    
+    System.out.println(aByte);    
+
+    }
+}
